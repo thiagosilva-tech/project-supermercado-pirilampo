e2861dafd7e7008a6b1daa564fc233b267c3f79f
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-07 14:58:15 -0300

    feat: resolução requisito 6

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,16 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductByName = (productSearched) => {
+    for (let index = 0; index < stockProducts.length; index += 1) {
+       if (stockProducts[index].productName === productSearched) {
+        const formattedPrice = `R$ ${(stockProducts[index].price).toFixed(2)}`
+        return {
+            description: stockProducts[index].description,
+            formattedPrice: formattedPrice
+        }
+       }        
+    }
+    return null;
 };
 
 module.exports = { searchProductByName };
