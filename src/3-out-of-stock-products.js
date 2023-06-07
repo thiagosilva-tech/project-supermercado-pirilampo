ace3308cd9fef92b7c47451e6eef18e15754a78f
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-07 13:47:45 -0300

    feat: resolução requisito 3

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -1,7 +1,16 @@
 const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+ const outOfStockProducts = [];
+
+ for (let index = 0; index < stockProducts.length; index += 1) {
+  if (stockProducts[index].quantityInStock == 0) {
+    outOfStockProducts.push(stockProducts[index].productName);
+  }
+ }
+ return outOfStockProducts;
 };
+
+console.log(getOutOfStockProducts());
 
 module.exports = { getOutOfStockProducts };
