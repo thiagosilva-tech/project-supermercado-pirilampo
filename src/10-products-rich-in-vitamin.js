7a96a234d50920ace431c6ff0d8c96df9f7f075a
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-07 17:41:16 -0300

    feat: resolução requisito 10

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,19 @@
 const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
+  const productsRichInVitamin = [];
+  for (let index = 0; index < stockProducts.length; index++) {
+    if (stockProducts[index].nutritionalInfo.vitamins != undefined) {
+      let formattedPrice = `R$ ${stockProducts[index].price.toFixed(2)}`;
+      let nutritionalInfoVitamins = Object.entries(stockProducts[index].nutritionalInfo.vitamins).map(([key, value]) => `${key} - ${value}`);
+      productsRichInVitamin.push({
+        description: stockProducts[index].description,
+        formattedPrice: formattedPrice,
+        vitaminsInformation: nutritionalInfoVitamins
+      })
+    }    
+  }
+  return  productsRichInVitamin;
 };
 
 module.exports = { getProductsRichInVitamin };
