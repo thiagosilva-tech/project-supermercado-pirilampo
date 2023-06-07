9ee2312a6e5da92726749d330ba21522aab39bf9
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-07 13:32:23 -0300

    feat: resolução requisito 2

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -1,7 +1,16 @@
 const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
-  // Desenvolva seu código dentro dessa função...
+  const uniqueProductsName = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (uniqueProductsName.length === 0) {
+      uniqueProductsName.push(stockProducts[index].productName);
+    } else if (!uniqueProductsName.includes(stockProducts[index].productName))
+    uniqueProductsName.push(stockProducts[index].productName);
+  }
+
+  return uniqueProductsName;
 };
 
 module.exports = { getUniqueProductsName };
