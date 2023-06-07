14ec1a319a32d52f5a4885951871c91c8cd35735
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-07 15:14:41 -0300

    feat: resolução requisito 8

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+  const productsOnSale = [];
+  for (let index = 0; index < stockProducts.length; index++) {
+    if (stockProducts[index].onSale === true) {
+      const formattedPrice = `R$ ${(stockProducts[index].price).toFixed(2)}`
+      productsOnSale.push({
+        description: stockProducts[index].description,
+        formattedPrice: formattedPrice,
+        onSale: stockProducts[index].onSale
+      });
+    };    
+  };
+  return productsOnSale;
 };
 
 module.exports = { getProductsOnSale };
