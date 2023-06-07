fbf03f615a792cbebf35658722f234686e4e2426
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-07 17:29:50 -0300

    feat: resolução requisito 9

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,27 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
+  const productsWithAllergyOrIntoleranceInfo = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    let formattedPrice = `R$ ${stockProducts[index].price.toFixed(2)}`;
+
+    if (stockProducts[index].allergyOrIntolerance != undefined) {
+      let formattedAllergyOrIntolerance = stockProducts[index].allergyOrIntolerance.join(' ').replace(/['"]/g, '');
+      let allergyOrIntoleranceMessage = `Pode conter: ${formattedAllergyOrIntolerance}`;
+      productsWithAllergyOrIntoleranceInfo.push({
+        description: stockProducts[index].description,
+        formattedPrice: formattedPrice,
+        allergyOrIntoleranceMessage: allergyOrIntoleranceMessage
+      });
+    } else {
+      productsWithAllergyOrIntoleranceInfo.push({
+        description: stockProducts[index].description,
+        formattedPrice: formattedPrice,
+      });
+    }
+  }
+  return productsWithAllergyOrIntoleranceInfo;
 };
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
