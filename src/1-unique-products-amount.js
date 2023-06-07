6bc753b412de697ad2811cd81e91bf4aeaa9d271
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-07 12:01:55 -0300

    fix: requisito 1

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -1,16 +1,19 @@
 const stockProducts = require('./data.json');
-const teste = {};
+
 const getUniqueProductsAmount = (data) => {
+  if(data === undefined){
+    return 0;
+  }
+
   let productsUniques = []; 
 
-  for (let i = 0; i < data.length; i++) {
-    if (productsUniques[0] === []) {
-      productsUniques = data[0].productName
+  for (let i = 0; i < data.length; i += 1) {
+    if (productsUniques.length === 0) {
+      productsUniques.push(data[i].productName); 
     } else if (!productsUniques.includes(data[i].productName)) {
       productsUniques.push(data[i].productName);
     }    
-  }  
-
+  }
   return productsUniques.length;
 };
 
