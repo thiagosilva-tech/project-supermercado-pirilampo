ae626843a14c313bf84b51a34a691de64cdeeeb8
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-07 17:55:36 -0300

    fix: requisito 1

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -1,20 +1,7 @@
 const stockProducts = require('./data.json');
 
-const getUniqueProductsAmount = (data) => {
-  if(data === undefined){
-    return 0;
-  }
-
-  let productsUniques = []; 
-
-  for (let i = 0; i < data.length; i += 1) {
-    if (productsUniques.length === 0) {
-      productsUniques.push(data[i].productName); 
-    } else if (!productsUniques.includes(data[i].productName)) {
-      productsUniques.push(data[i].productName);
-    }    
-  }
-  return productsUniques.length;
+const getUniqueProductsAmount = () => {
+    return stockProducts.length; 
 };
 
 module.exports = { getUniqueProductsAmount };
